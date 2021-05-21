d185fcd07db94961c90f4f83cf55bc5ac5bd3dfc
Artur <79041911+Fateyv@users.noreply.github.com>
2021-05-21 13:52:20 +0300

    added all

---
+++ script.js
@@ -0,0 +1,44 @@
+const students = [
+    {
+        id:10,
+        name: 'John Smith',
+        marks: [10, 8, 6, 9, 8, 7 ]
+    },
+    {
+        id:11,
+        name: 'John Doe',
+        marks: [ 9, 8, 7, 6, 7 ]
+    },
+    {
+        id:12,
+        name: 'Thomas Anderson',
+        marks: [6, 7, 10, 8 ]
+    },
+    {
+        id:13,
+        name: 'Jean-Baptiste Emanuel Zorg',
+        marks: [10, 9, 8, 9 ]
+    }
+]
+
+
+function averageStudentMark(i) {
+    let marksLenght = 0;
+    let sumStudentMarks = students[i].marks.reduce((sum, item) => (sum + item));
+    marksLenght = students[i].marks.length;
+    return (
+        sumStudentMarks / marksLenght
+    )
+}
+
+function averageGroupMark(students) {
+    let sum = 0;
+    let allmarksLenght = 0;
+    students.forEach((Student) => (allmarksLenght += Student.marks.length))
+    students.forEach((Student) => (Student.marks.forEach((mark) => sum += mark)));
+    return sum / allmarksLenght;
+}
+
+const averageGroup = averageGroupMark(students);
+const averageMark = averageStudentMark(2);
+
